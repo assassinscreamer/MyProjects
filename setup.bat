4a918eedc7445c414b884dea5b35972d9ae73937
assassinscreamer <61639907+assassinscreamer@users.noreply.github.com>
2021-03-05 20:25:23 +0100

    Setup.bat
    this script is a simple batch file set whose main purpose is economy
    its role is very simple: it detects the downloads and stops the PC after the end of these
    this script is created by AYMAN BADDOU
    NB : the language of this script is French

---
+++ setup.bat
@@ -0,0 +1,474 @@
+@echo off
+
+rem **********************************************MENU************************************************************************************
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+echo.                                                                                                           
+echo                                         ^< ^< ^< TimeShut By Ayman Baddou ^> ^> ^>                                       
+echo.                                                                                                         
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+ping 192.0.2.2 -n 1 -w 100 > nul                                                                       
+cls
+                                                         
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+echo.
+echo                                            ^<^<^<TimeShut By Ayman Baddou^>^>^>
+echo.
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+ping 192.0.2.2 -n 1 -w 100 > nul
+cls
+
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+echo.
+echo                                           ^< ^<^<TimeShut By Ayman Baddou^>^> ^>
+echo.
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping 192.0.2.2 -n 1 -w 100 > nul
+cls
+
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+echo.
+echo                                            ^<^<^<TimeShut By Ayman Baddou^>^>^>
+echo.
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+ping 192.0.2.2 -n 1 -w 100 > nul
+cls
+
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+
+echo.
+echo                                           ^<^< ^<TimeShut By Ayman Baddou^> ^>^>
+echo.
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping 192.0.2.2 -n 1 -w 100 > nul
+cls
+
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+echo.
+echo                                            ^<^<^<TimeShut By Ayman Baddou^>^>^>
+echo.
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+ping 192.0.2.2 -n 1 -w 100 > nul
+cls
+
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+echo.
+echo                                         ^< ^< ^< TimeShut By Ayman Baddou ^> ^> ^>
+echo.
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping 192.0.2.2 -n 1 -w 100 > nul
+cls
+
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+echo.
+echo                                            ^<^<^<TimeShut By Ayman Baddou^>^>^>
+echo.
+echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+ping 192.0.2.2 -n 1 -w 2000 > nul
+
+set telefin=%USERPROFILE%\Downloads
+if exist "%USERPROFILE%\program files\*.*" (goto exchek)
+goto menu
+:teleprof
+cls
+echo voulez vous definir le chemin suivant comme repertoire de telechargement ?
+echo "%USERPROFILE%\Downloads"
+set /p teleanw=">>"
+if %teleanw%==oui (goto menuyes)
+if %teleanw%==non (goto nonchn)
+echo stp "oui" ou "non"
+pause
+goto teleprof
+:menu
+cls
+set /p play="voulez vous installer TimeShut:"
+if %play%==oui (goto teleprof)
+if %play%==non (echo ok le script va se fermer
+timeout 5
+exit
+)
+echo stp "oui" ou "non"
+pause
+cls
+goto menu
+:menuyes
+cls
+echo charger la notice d'utiisation ? (importante pour la premiere utilisation)
+set /p not=">>"
+if %not%==oui (goto insnote)
+if %not%==non (goto insbegin)
+echo stp "oui" ou "non"
+pause
+goto menuyes
+:insbegin
+cls
+echo chargement de fichiers en cours
+echo veillez patientez...
+timeout /NOBREAK /t 5 > nul
+cls
+rem ***********************************************Créer le timeout**************************************************
+
+if not exist "%USERPROFILE%\program files" (MD "%USERPROFILE%\program files")
+set chem=%USERPROFILE%\program files\run timeout.bat
+echo @echo off > "%chem%"
+echo echo 1 telechargement a ete ajoute >> "%chem%"
+echo echo. >> "%chem%"
+echo echo voulez vous planifier un arret automatique apres la fin de telechargement (repondre seulement "oui" ou "non") ? >> "%chem%"
+echo :loop >> "%chem%"
+echo set /p anwser= >> "%chem%"
+echo if %%anwser%% == oui (goto run) >> "%chem%"
+echo if %%anwser%% == non (goto exit) >> "%chem%"
+echo echo stp seulement "oui" ou "non" >> "%chem%"
+echo goto loop >> "%chem%"
+echo :exit >> "%chem%"
+echo echo ok le script va se fermer >> "%chem%"
+echo timeout 2 >> "%chem%"
+echo exit >> "%chem%"
+echo :run >> "%chem%"
+echo cls >> "%chem%"
+echo if exist "%telefin%\*.crdownload" (echo telechargement en cours) >> "%chem%"
+echo if not exist "%telefin%\*.crdownload" ( >> "%chem%"
+echo echo tous les telechargement sont termines >> "%chem%"
+echo goto finish) >> "%chem%"
+echo timeout 1 ^> nul >> "%chem%"
+echo goto run >> "%chem%"
+echo :finish >> "%chem%"
+echo echo le pc va s'arreter dans 30 secondes >> "%chem%"
+echo shutdown -s -hybrid -t 30 >> "%chem%"
+echo timeout 5 >> "%chem%"
+echo exit >> "%chem%"
+
+rem ***************************************************creer le checker*********************************************************
+
+echo @echo off > "%USERPROFILE%\program files\Download checker.bat"
+
+echo set down=0 >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo :loop >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo if %%down%% == 0 ( >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo if exist "%telefin%\*.crdownload" (goto start) >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo ) >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo if not exist "%telefin%\*.crdownload" (set down=0) >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo timeout 1 >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo cls >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo echo pas de nouveau telechargement >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo goto loop >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo :start >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo start "" "%USERPROFILE%\program files\run timeout.bat" >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo set /a down+=1 >> "%USERPROFILE%\program files\Download checker.bat"
+
+echo goto loop >> "%USERPROFILE%\program files\Download checker.bat"
+
+rem ***************************************************Creer le Vbs***********************************************************
+
+echo Set oWShell = CreateObject("Wscript.Shell") > "%USERPROFILE%\program files\Hide bat.vbs"
+echo oWShell.Run """%USERPROFILE%\program files\Download checker.bat""", 0, False >> "%USERPROFILE%\program files\Hide bat.vbs"
+echo Set oWSHell = Nothing >> "%USERPROFILE%\program files\Hide bat.vbs"
+cls
+
+rem ******************************************creer un declencheur***************************************************
+:con
+set script=TimeShut.bat
+
+echo @echo off> "%script%"
+
+echo if exist "%telefin%\test.crdownload" (del %telefin%\test.crdownload) >>"%script%"
+
+echo cls >>"%script%"
+
+echo tasklist ^| findstr /c:timeout.exe ^> nul>> "%script%"
+
+echo if %%errorlevel%%==0 (goto exdj)>> "%script%"
+
+echo cls >>"%script%"
+
+echo :quest >>"%script%"
+
+echo cls>>"%script%"
+
+echo echo voulez vous executer le script ?>>"%script%"
+
+echo set /p anw=">>">>"%script%"
+
+echo if %%anw%%==oui (goto anwyes)>>"%script%"
+
+echo if %%anw%%==non (echo ok le script va se fermer>>"%script%"
+
+echo timeout 5 >>"%script%"
+
+echo exit)>>"%script%"
+
+echo echo stp "oui" ou "non">>"%script%"
+
+echo timeout 5 >>"%script%"
+
+echo goto quest>>"%script%"
+
+echo :anwyes>>"%script%"
+
+echo cls >>"%script%"
+
+echo set fin=0 >> "%script%"
+
+echo echo merci de patienter >> "%script%"
+
+echo :loop>> "%script%"
+
+echo tasklist ^| findstr /c:timeout.exe ^> nul>> "%script%"
+
+echo if %%errorlevel%%==0 (goto exdj)>> "%script%"
+
+echo set /a fin+=1 >> "%script%"
+
+echo if %%fin%%==20 (goto continue)>> "%script%"
+
+echo goto loop>> "%script%"
+
+echo :continue>> "%script%"
+
+echo cls >>"%script%" 
+
+echo start "" "%USERPROFILE%\program files\Hide bat.vbs">> "%script%"
+
+echo echo le programme a ete executee avec succes>> "%script%"
+
+echo pause>> "%script%"
+
+echo exit>> "%script%"
+
+echo :exdj >> "%script%"
+
+echo cls >> "%script%"
+
+echo echo le script est deja executee >> "%script%"
+
+echo echo arreter ou tester ? >> "%script%"
+
+echo echo 1 :: arreter >> "%script%"
+
+echo echo 2 :: tester >> "%script%"
+
+echo set /p areanw=">>" >> "%script%"
+
+echo if %%areanw%%==1 (goto arret) >> "%script%"
+
+echo if %%areanw%%==2 (goto tester) >> "%script%"
+
+echo echo choix incorrect reesayer >> "%script%"
+
+echo pause>> "%script%"
+
+echo goto exdj >> "%script%"
+
+echo :arret >> "%script%"
+
+echo echo fermeture du script >> "%script%"
+
+echo MD "%%USERPROFILE%%\program files\stop" >> "%script%"
+
+echo move /Y "%%USERPROFILE%%\program files\*.bat" "%%USERPROFILE%%\program files\stop" >> "%script%"
+
+echo cls >> "%script%"
+
+echo echo fermeture du script >> "%script%"
+
+echo timeout 3 ^> nul >> "%script%"
+
+echo move /Y "%%USERPROFILE%%\program files\stop\*.bat" "%%USERPROFILE%%\program files" >> "%script%"
+
+echo cls >> "%script%"
+
+echo echo le script a ete ferme>> "%script%"
+
+echo timeout 5 >> "%script%"
+
+echo exit >> "%script%"
+
+echo :sortie >> "%script%"
+
+echo echo le script va se fermer >> "%script%"
+
+echo timeout 5 >> "%script%"
+
+echo exit >> "%script%"
+
+echo :tester >> "%script%"
+
+echo echo le repertoire de detection de telechargement est le suivant : "%telefin%" >> "%script%"
+
+echo echo si vous voulez le changer reexecutez Setup.bat >> "%script%"
+
+echo echo appuier sur une touche pour demarer le test >> "%script%"
+
+echo pause ^> nul >>"%script%"
+
+echo cls >> "%script%"
+
+echo echo le test a demarre >> "%script%"
+
+echo timeout 2 ^> nul >> "%script%"
+
+echo cls >> "%script%"
+
+echo echo apres 15 secondes vous devez voir un fenetre cmd executee >> "%script%"
+
+echo echo continuer la configuration en tapant "oui" >> "%script%"
+
+echo echo revenez ici apres voir "le telechargement en cours" >> "%script%"
+
+echo timeout 15 >> "%script%"
+
+echo echo le test a commencer ^>^> "%telefin%\test.crdownload" >> "%script%"
+
+echo pause >> "%script%"
+
+echo echo le test a termine >> "%script%"
+
+echo echo si vous verrez la fenetre cmd, tous fonctionne bien >> "%script%"
+
+echo echo sinon, reinsstaller le programme >> "%script%"
+
+echo del %telefin%\test.crdownload >> "%script%" 
+
+echo timeout 5 ^> nul >> "%script%"
+
+echo shutdown -a >> "%script%"
+
+echo echo la fermeture de sesion a ete annulee automatiquement>> "%script%"
+
+echo pause >> "%script%"
+
+echo exit >> "%script%"
+
+goto setfin
+
+rem ********************************************dossier de telechargement*********************************************************
+:setfin
+echo tous les fichier sont chargee et pres pour l'execution
+echo lire la notice d'utilisation avant faire quelque chose (si c'est la premiere fois)
+timeout 30
+exit
+:insnote
+rem *************************************************creer la notice***************************************************
+echo Information sur le script:> "Notice d'utilisation.txt" 
+echo ce script est un ensemble de fichier bat simple dont son but principale est l'economie>> "Notice d'utilisation.txt" 
+echo sont role est tres simple: il detecte les telechargements et arrete le PC apres la fin de ces derniers>> "Notice d'utilisation.txt" 
+echo ce script est cree par AYMAN BADDOU>> "Notice d'utilisation.txt" 
+echo Si vous avez des questions ou des problemes n'hesitez pas a nous contacter >> "Notice d'utilisation.txt" 
+echo Pour contacter le centre Client appeler : 0529957076 >> "Notice d'utilisation.txt" 
+echo.>> "Notice d'utilisation.txt"  
+echo Foire aux questons: >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo 1 :: Comment executer le programme ? >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo après avoir executer le "setup.bat" vous verrez un fichier "TimeShut.bat" >> "Notice d'utilisation.txt"
+echo vous devez executez ce bat >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo 2 :: Je ne vois rien changee ? >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo meme si vous ne verrez rien changee assurrez vous que le programme a ete lance en arriere plan >> "Notice d'utilisation.txt
+echo. >> "Notice d'utilisation.txt"
+echo 3 :: Comment tester ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo pour le tester reouvrit TimeShut.bat et il va oropser le test  >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo   4-1 :: J'ai supprime des fichiers bat ! Que dois-je faire ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo en cas de suppression de "TimeShut.bat" vous devez reexecuter le fichier setup.bat pour le recharger >> "Notice d'utilisation.txt"
+echo si vous les supprimez tous les deux vous devez retelecharger le setup.bat >> "Notice d'utilisation.txt"
+echo.>> "Notice d'utilisation.txt"
+echo 5 :: C'est quoi le repertoire de telechargement ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo le repertoire de telechargement n'est pas le repertoire d'installation du programme>> "Notice d'utilisation.txt"
+echo c'est le repertoire ou le script va detecter le telechargement de chrome (%USERPROFILE%\Downloads) par defaut>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo 6 :: Moi j'ai pas "%USERPROFILE%\Downloads" comme repertoire de telechargement ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo dans "setup.bat" il propose le chemin suivant "%USERPROFILE%\Downloads" comme repertoire par defaut>> "Notice d'utilisation.txt"
+echo mais il est possible de changer ce repertoire en tapant "non" pour specifier votre chemin personnalisé>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo 7 :: Je veux changer le repertoire de telechargement apres avoir installer TimeShut.bat. >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo vous pouvez changer ce repertoire en reexecutant "setup.bat" et choisir l'option "2 :: reparer">> "Notice d'utilisation.txt"
+echo et il va proposer a nouveau de choisir le repertoire de telechargement>> "Notice d'utilisation.txt" 
+echo. >> "Notice d'utilisation.txt"
+echo 8 :: Je debute un telechargement mais rien ne se passe ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo assurez vous de telecharger avec "google chrome" et que ce fichier deppasse 15 MB>> "Notice d'utilisation.txt"
+echo assurez vous de telecharger le fichier dans le repertoire choisit par vous dans le setup.bat>> "Notice d'utilisation.txt"
+echo lors du choix de repertoire de telechargement personnalisé assurez d'eliminer le dernier slash "\"  >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo Exemple : >> "Notice d'utilisation.txt"
+echo au lieu d'ecrire "C:\users\test\"... vous devez ecrire "C:\users\test"...>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo 9 :: comment arreter le script ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo apres avoir executer le script (depuis "TimeShut.bat") ce dernier est executee en arriere plan>> "Notice d'utilisation.txt"
+echo pour l'arreter relancer "TimeShut.bat" . Il va detecter l'execution du script>> "Notice d'utilisation.txt"
+echo et il va proposer de l'arreter (le script)>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo 10 :: comment desinstaller "TimeShut.bat" ?>> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo pour le desinstaller reexecutez "setup.bat". Il va detecter la presence de script>> "Notice d'utilisation.txt"
+echo et il va proposer la reparation ou la desinstallation>> "Notice d'utilisation.txt" 
+echo choisir "1 :: desinstaller" pour le desinstaller >> "Notice d'utilisation.txt" 
+echo.>> "Notice d'utilisation.txt"
+echo Cordialement >> "Notice d'utilisation.txt"
+echo. >> "Notice d'utilisation.txt"
+echo By Ayman Baddou  >> "Notice d'utilisation.txt"
+goto insbegin
+:nonchn
+cls
+echo merci d'ecrire le repertoire de telechargement(eliminer le dernier slash:"\")
+set /p teledoss=">>"
+if not exist "%teledoss%\" (echo repertoire introuvable merci de reesayer
+pause
+goto nonchn)
+set telefin=%teledoss%
+echo le repertoire "%telefin%\" est definit comme dossier de telechargement
+pause
+goto menuyes
+:destal
+cls
+echo voulez vous vraiment desinstaller timeshut ?
+set /p desanw=">>"
+if %desanw%==non (goto exchek)
+if %desanw%==oui (
+echo desinstallation du prog en cours
+echo veillez patienter...
+timeout 3 > nul
+del /Q "%USERPROFILE%\program files"
+del /Q "%cd%\TimeShut.bat"
+del /Q "%cd%\Notice d'utilisation.txt"
+rmdir /S /Q "%USERPROFILE%\program files"
+cls
+echo tous les fichiers sont dessinstallee avec succes
+pause
+exit
+)
+echo stp "oui" ou "non"
+pause
+goto destal
+:exchek
+cls
+echo le prog est deja installe
+echo que voulez vous faire ?
+echo 1 :: Desinstaller
+echo 2 :: reparer
+set /p desrep=">> "
+if %desrep%==1 (goto destal)
+if %desrep%==2 (goto teleprof)
+echo choix incorrecte
+pause
+goto exchek
